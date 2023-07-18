780a5fc81bc5b9bccde19e6a7ef829c58aca53d5
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-07-18 23:18:40 +0900

    [Silver II] Title: 섬의 개수, Time: 460 ms, Memory: 36424 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,63 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	static int w,h;
+	static int[][] map;
+	static int cnt;
+	static int[] dx = {-1,1,0,0,-1,-1,1,1};
+	static int[] dy = {0,0,-1,1,-1,1,-1,1}; //상하좌우 좌상우상좌하우하
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<>();
+		while(true) {
+			w = sc.nextInt();
+			h = sc.nextInt();
+			
+			if(w == 0 && h == 0) {
+				break;
+			}
+			map = new int[h][w];
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}
+			cnt = 0;
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					if(map[i][j] == 1) {
+						map[i][j] = 0;
+						dfs(i,j);
+						cnt++;
+					}
+				}
+			}
+			list.add(cnt);
+		}
+		for(Integer i : list) {
+			System.out.println(i);
+		}
+	}
+	public static void dfs(int a, int b) {
+		for(int i=0; i<8; i++) {
+			int nextX = a + dx[i];
+			int nextY = b + dy[i];
+			
+			if(nextX < 0 || nextY < 0 || nextX >= h || nextY >= w) { // 범위 나가면 무시
+				continue;
+			}
+			
+			if(map[nextX][nextY] == 0) { // 바다면 무시
+				continue;
+			}
+			
+			map[nextX][nextY] -= 1;
+			dfs(nextX, nextY);
+			
+		}
+	}
+
+}
