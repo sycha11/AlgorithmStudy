bd527c2a64c52bb9e945e92035952e63d9dc3b5b
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-07-25 12:53:26 +0900

    [Silver IV] Title: 수 찾기, Time: 2072 ms, Memory: 187264 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,44 +1,45 @@
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++) {
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
 			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
+		int M = sc.nextInt();
+		int[] answer = new int[M];
 		
-		int m = sc.nextInt();
+		for(int i=0; i<M; i++) {
+			int find = sc.nextInt();
 			
-		for(int i=0; i<m; i++) {
-			boolean find = false;
-			int target = sc.nextInt();
 			int start = 0;
-			int end = arr.length -1;
-			
+			int end = arr.length - 1;
 			while(start <= end) {
-				int midi = (start + end) / 2;
-				int midV = arr[midi];
-				if(midV > target) {
-					end = midi -1;
-				} else if(midV < target) {
-					start = midi + 1;
+				
+				int mid = (start+end)/2;
+				
+				if(arr[mid] > find) {
+					end = mid - 1;
+				} else if(arr[mid] < find) {
+					start = mid + 1;
 				} else {
-					find = true;
+					answer[i] = 1;
 					break;
 				}
+				
 			}
-			if(find) {
-				System.out.println(1);
-			} else {
-				System.out.println(0);
-			}
+		}
+		
+		for(int i=0; i<answer.length; i++) {
+			System.out.println(answer[i]);
 		}
 	}
 
