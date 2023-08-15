e38b60720efa847df24f99a331b883011cc970d9
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-08-15 14:10:34 +0900

    [Silver III] Title: 소수 구하기, Time: 904 ms, Memory: 36956 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int[] A = new int[b+1];
+		
+		for(int i=2; i<=b; i++) {
+			A[i] = i;
+		}
+		
+		for(int i=2; i<=b; i++) {
+			if(A[i] == 0) {
+				continue;
+			}
+			for(int j = i+i; j<=b; j+=i) {
+				A[j] = 0;
+			}
+		}
+		
+		for(int i=a; i<=b; i++) {
+			if(A[i] != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
