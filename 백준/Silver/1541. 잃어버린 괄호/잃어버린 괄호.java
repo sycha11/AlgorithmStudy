ed2eb525ce517393d03d71ce3db4d74b242d0bcb
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-08-20 23:06:37 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 160 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
 
@@ -37,5 +36,4 @@ public class Main {
 		System.out.println(sum);
 		
 	}
-
 }
