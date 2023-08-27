42566fc79038c7e187f7bb6302dc734218957ee1
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-08-27 17:29:42 +0900

    [Gold IV] Title: 여행 가자, Time: 132 ms, Memory: 15640 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n,m;
+	static int[] parents;
+	
+	private static void make() {
+		parents = new int[n];
+		for(int i=0; i<n; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	private static int find(int a) {
+		if(a == parents[a]) return a;
+		return parents[a] = find(parents[a]);
+	}
+	
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		make();
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<n; j++) {
+				int con = Integer.parseInt(st.nextToken());
+				if(con == 1) {
+					union(i,j);
+				}
+			}
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int[] visited = new int[m];
+		boolean flag = true;
+		
+		for(int i=0; i<m; i++) {
+			visited[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i=0; i<m-1; i++) {
+			int idx = visited[i]-1; // 012
+			int nidx = visited[i+1] -1;
+			if(find(idx) != find(nidx)) {
+				flag = false;
+			}
+		}
+		if(flag) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+		
+	}
+
+}
