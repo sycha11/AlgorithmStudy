749f52f3812146d4ef86a2b5b94af2a642fcf1c4
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-08-29 15:58:23 +0900

    [Silver I] Title: RGB거리, Time: 144 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,25 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int[][] c = new int[num][3];
 
-        for (int i = 0; i < num; i++) {
-            for (int j = 0; j < 3; j++) {
-                c[i][j] = scanner.nextInt();
-            }
-        }
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		int[][] color = new int[n][3];
+		int[][] dp = new int[n+1][3];
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<3; j++) {
+				color[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dp[0][0] = color[0][0];
+		dp[0][1] = color[0][1];
+		dp[0][2] = color[0][2];
+		
+		for(int i=1; i<n; i++) {
+			dp[i][0] = color[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
+			dp[i][1] = color[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
+			dp[i][2] = color[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+		}
+		
+		System.out.println(Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2])));
+	}
 
-        int[][] dp = new int[num][3];
-        for (int i = 0; i < num; i++) {
-            dp[i][0] = c[i][0] + ((i > 0) ? Math.min(dp[i-1][1], dp[i-1][2]) : 0);
-            dp[i][1] = c[i][1] + ((i > 0) ? Math.min(dp[i-1][0], dp[i-1][2]) : 0);
-            dp[i][2] = c[i][2] + ((i > 0) ? Math.min(dp[i-1][1], dp[i-1][0]) : 0);
-        }
-
-        int result = Math.min(dp[num-1][0], Math.min(dp[num-1][1], dp[num-1][2]));
-        System.out.println(result);
-    }
 }
