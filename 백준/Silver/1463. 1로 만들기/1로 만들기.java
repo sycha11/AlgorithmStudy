e1a07a47c3a66933670f10137212aafb3296b6f9
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-08-29 14:43:42 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,27 +1,36 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] dp;
-	static int x;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		x = sc.nextInt();
-		dp = new int[x+1];
-		
-		dp[1] = 0;
-		for(int i=2; i<=x; i++) {
-			dp[i] = dp[i-1] +1;
-			if(i%2==0) {
-				dp[i] = Math.min(dp[i], dp[i/2]+1);
-			}
-			if(i%3==0) {
-				dp[i] = Math.min(dp[i], dp[i/3]+1);
-			}
+	static int[] memo;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		System.out.println(dp(n));
+	}
+	
+	public static long dp(int n) {
+		memo = new int[n+1];
+		for (int i = 0; i < memo.length; i++) {
+			memo[i] = 0;
 		}
-		System.out.println(dp[x]);
+		
+		
+		for (int i = 2; i < n+1; i++) {
+			memo[i] = memo[i-1] +1;
+			if (i%2==0) {
+				memo[i] = Math.min(memo[i],memo[i/2]+1);
+			}
+			if (i%3==0) {
+				memo[i] = Math.min(memo[i],memo[i/3]+1);
+			}	
+		}
+		return memo[n];
 	}
 
 }
