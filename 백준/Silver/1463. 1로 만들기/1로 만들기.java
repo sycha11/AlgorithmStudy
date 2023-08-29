9fe4ed151bfe5b04398140779764ce8c071c6999
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-08-29 17:24:10 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,36 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] memo;
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		System.out.println(dp(n));
-	}
-	
-	public static long dp(int n) {
-		memo = new int[n+1];
-		for (int i = 0; i < memo.length; i++) {
-			memo[i] = 0;
-		}
 		
+		int n = Integer.parseInt(br.readLine());
 		
-		for (int i = 2; i < n+1; i++) {
-			memo[i] = memo[i-1] +1;
-			if (i%2==0) {
-				memo[i] = Math.min(memo[i],memo[i/2]+1);
+		int[] dp = new int[n+1];
+		
+		dp[1] = 0;
+		for(int i=2; i<=n; i++) {
+			dp[i] = dp[i-1] + 1; // 1로 빼는 경우
+			
+			if(i%3 == 0) { // 3으로 나누어지는 경우
+				dp[i] = Math.min(dp[i/3]+1, dp[i]);
 			}
-			if (i%3==0) {
-				memo[i] = Math.min(memo[i],memo[i/3]+1);
-			}	
+			if(i%2 == 0) { // 2로 나누어지는 경우
+				dp[i] = Math.min(dp[i/2]+1, dp[i]);
+			}
+			
 		}
-		return memo[n];
+		System.out.println(dp[n]);
+		
+		
 	}
 
+	
 }
