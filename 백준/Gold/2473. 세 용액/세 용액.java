f5c27aea19d929163e9f4e2dff78a414d13b1fdd
sycha11 <76770475+sycha11@users.noreply.github.com>
2023-12-15 18:31:16 +0900

    [Gold III] Title: 세 용액, Time: 276 ms, Memory: 16516 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long[] arr = new long[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 정렬
+		Arrays.sort(arr);
+		long min = Long.MAX_VALUE;
+		long[] answer = new long[3];
+		
+		for(int i=0; i<n-2; i++) {
+			int left = i+1;
+			int right = n-1;
+			while(left < right) {
+				long val = (long) Math.abs(arr[i] + arr[left] + arr[right]);
+				if(min > val) {
+					min = val;
+					answer[0] = arr[i];
+					answer[1] = arr[left];
+					answer[2] = arr[right];
+				}
+//				System.out.println(arr[i] +" " + arr[left] + " " + arr[right]);
+				if(Math.abs(arr[i] + arr[left+1] + arr[right]) < Math.abs(arr[i] + arr[left] + arr[right-1])) {
+					left++;
+				} else {
+					right--;
+				}
+			}
+		}
+		Arrays.sort(answer);
+		
+		for(int i=0; i<answer.length; i++) {
+			System.out.print(answer[i] + " ");
+		}
+	}
+
+}
