00753a96f4790dc8b84de7034eb30ad37ba86ae5
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-03-03 21:52:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 68.37 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -8,57 +8,61 @@ class Solution {
         Queue<Integer> q2 = new LinkedList<>();
         
         long sum1 = 0;
-        long total = 0;
+        long sum2 = 0;
+        
+        int max1 = 0;
+        int max2 = 0;
+        int max = 0;
         
         for(int i=0; i<queue1.length; i++){
-            total += queue1[i] + queue2[i];
-            sum1 += queue1[i];
             q1.add(queue1[i]);
             q2.add(queue2[i]);
         }
-       
-        // sum1 = qhap(q1);
-        // sum2 = qhap(q2);
-        System.out.println(sum1);
+        
+        
+        sum1 = qhap(q1);
+        sum2 = qhap(q2);
+        // System.out.println(sum1);
+        // System.out.println(sum2);
 
-        int size = (q1.size() + q2.size()) * 2;
-        System.out.println(size);
-        // int result = size;
+        int size = (q1.size() + q2.size()) * 3;
+        int result = size;
             
         int cnt = 0;
+        long total = sum1 + sum2;
         
-        // max = Math.max(max1, max2);
         long target = total/2; // 15
         
-        // 총 원소의 합이 홀수면 -1
+        // 총 원소의 합이 홀수거나 가장 큰 원소의 값이 total의 절반보다 크다면 계산 할 수가 없다.
         if(total % 2 != 0) {
             return -1;
         }
         
          while(true){
-             if(answer>size){
+            if(answer>size){
                  return -1;
              }
-            if(sum1 == target){
-                break;
-            } else if(sum1 > target){
-                sum1 -= q1.peek();
-                int a = q1.poll();
-                q2.add(a);
-            } else {
-                sum1 += q2.peek();
+            if(target < sum2){ // q2가 더 크다면
+                // q2에서 빼서 q1에 넣어줌
+                sum2 -= q2.peek();
                 int a = q2.poll();
                 q1.add(a);
+            } else if(target > sum2){ // q2가 작다면
+                // q1에서 빼서 q2에 넣어줌
+                sum2 += q1.peek();
+                int a = q1.poll();
+                q2.add(a);
+            } else { // 같다면
+                // 같다면 끝
+                break;
             }
 
             // sum1 = qhap(q1);
             // sum2 = qhap(q2);
-
+            size--;
             answer++;
         }
         
-        
-       
         return answer;
     }
     
