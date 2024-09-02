cf06dfd3c4978ca2797235e1bef5f95d88de16b8
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-09-02 14:46:59 +0900

    [Silver III] Title: N과 M (1), Time: 1760 ms, Memory: 61808 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,36 +1,39 @@
-
-
 import java.util.Scanner;
 
 public class Main {
 
-	public static int[] arr;
-	public static boolean[] visit;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 1~n
-		int M = sc.nextInt(); //  중복없이 찾는 숫자
-		
-		visit = new boolean[N];
-		arr = new int[M];
-		dfs(N,M,0);
-	}
-	
-	public static void dfs(int N, int M, int depth) {
-		if(depth == M) {
-			for(int val : arr) {
-				System.out.print(val + " ");
-			}
-			System.out.println();
-			return;
-		}
-		for(int i=0; i<N; i++) {
-			if(visit[i] == false) {
-				visit[i] = true;
-				arr[depth] = i+1;
-				dfs(N, M, depth +1);
-				visit[i] = false;
-			}
-		}
-	}
+    static int n,m;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n  = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[m];
+        visited = new boolean[n];
+
+        combi(0,0);
+    }
+    static void combi(int start, int cnt){
+        if(cnt == m){
+            for(int i=0; i<m; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]) {
+                arr[cnt] = i + 1;
+                visited[i] = true;
+                combi(start + 1, cnt + 1);
+                visited[i] = false;
+            }
+
+        }
+    }
 }
