b536d0bbfb0105e20cee89ddb011240fb6133158
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-09-02 15:24:39 +0900

    [Silver III] Title: N과 M (2), Time: 200 ms, Memory: 18356 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,32 +2,32 @@ import java.util.Scanner;
 
 public class Main {
 
-	static int N,M;
-	static int[] arr;
-	public static void main(String[] args) {
+    static int n,m;
+    static int[] arr;
 
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		arr = new int[M];
-		recu(1,0);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
 
-	private static void recu(int start, int depth) {
-		if(depth == M) {
-			for(int i : arr) {
-				System.out.print(i + " ");
-			}
-			System.out.println();
-			return;
-		}
-		for(int i=start; i<=N; i++) {
-			arr[depth] = i;
-			recu(i+1, depth+1);
-		}
-		
-	}
-	
+        arr = new int[m];
+
+        combi(1,0);
+
+    }
+
+    static void combi(int start, int cnt){
+        if(cnt == m){
+            for(int i=0; i<m; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=start; i<=n; i++){
+                arr[cnt] = i;
+                combi(i+1, cnt+1);
+        }
+    }
 }
