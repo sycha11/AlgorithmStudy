6b7083f26300e7b38604f5da12c5d742284dfd9c
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-10-14 14:20:47 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,33 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int n,m;
     static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         arr = new int[m];
+        visited = new boolean[n+1];
 
-        combi(1,0);
 
+        recu(1,0);
+        System.out.println(sb);
     }
 
-    static void combi(int start, int cnt){
-        if(cnt == m){
-            for(int i=0; i<m; i++){
-                System.out.print(arr[i] + " ");
+    private static void recu(int start, int depth){
+        if(depth == m){
+            for(int val : arr){
+                sb.append(val).append(' ');
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
         for(int i=start; i<=n; i++){
-                arr[cnt] = i;
-                combi(i+1, cnt+1);
+            arr[depth] = i;
+            recu(i+1, depth+1);
         }
     }
 }
