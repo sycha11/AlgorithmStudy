6e5505a90e1bdcaa66f01c48621167ee0498da94
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-10-15 13:52:08 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 17380 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,55 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int K,N;
-    static long MAX = 0;
-
-    public static void main(String[] args) throws IOException {
+    static int[] lan;
+ // 시간복잡도 생각하기
+ public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         K = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[K];
+        lan = new int[K];
 
+        long max = 0;
         for(int i=0; i<K; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-            if(MAX < arr[i]){
-                MAX = arr[i];
-            }
+            lan[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, lan[i]);
         }
-        Arrays.sort(arr);
 
-        System.out.println(upperBound(arr)-1);
-    }
+        long start = 0;
+        long end = max+1;
+        long answer = 0;
 
-    // 찾고자 하는 값보다 1큰 값을 리턴
-    private static long upperBound(int[] arr){
-        long lo = 0;
-        long hi = MAX+1;
+        while(start < end){
 
-        while(lo < hi){
-            long mid = (lo+hi) / 2;
-            long count = 0;
+            long mid = (start + end) / 2;
+            int sum = 0; // 잘라내는 갯수의 함 즉 답
 
-            for(int i=0; i<arr.length; i++){
-                count += arr[i] / mid;
+            for(int i=0; i<K; i++){
+                sum += lan[i] / mid;
             }
 
-            // 201이면 10개 200이면 11개
-            if(count < N){
-                hi = mid;
-            } else{
-                lo = mid +1;
+//            System.out.println(start + " " + end + " " + sum + " mid : " + mid);
+
+            if(sum >= N){
+                start = mid + 1;
+            } else { // 필요한 랜선 수 보다 부족하다면 Sum < N
+                end = mid;
             }
+
         }
-        return lo;
+
+        System.out.println(start - 1);
     }
 }
