ee08885fed06f004641f33faa2ebd354deb04367
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-10-17 11:44:34 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 272.36 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,6 +1,18 @@
 import java.util.*;
-import java.awt.Point;
 class Solution {
+    
+    static class Node implements Comparable<Node>{
+        int r,c,data;
+        public Node(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+        
+        @Override
+        public int compareTo(Node o){
+            return this.data - o.data;
+        }
+    }
     
     int[] dx = {-1,1,0,0}; // 상하좌우
     int[] dy = {0,0,-1,1};
@@ -8,65 +20,13 @@ class Solution {
     public int solution(int[][] points, int[][] routes) {
         int answer = 0;
         
-        Map<Integer, List<Point>> map = new HashMap<>();
-        
-        for(int i = 0; i < routes.length; i++){
-            int level = 1;
-            for(int j = 1; j < routes[i].length; j++){
-                int currentX = points[routes[i][j - 1] - 1][0];
-                int currentY = points[routes[i][j - 1] - 1][1];
-                int targetX = points[routes[i][j] - 1][0];
-                int targetY = points[routes[i][j] - 1][1];
-                
-                while(currentX != targetX){
-                    List<Point> pList = map.getOrDefault(level, new ArrayList<>());
-                    pList.add(new Point(currentX, currentY));
-                    map.put(level++, pList);
-                    
-                    if(currentX > targetX){
-                        currentX--;
-                    } else{
-                        currentX++;
-                    }
-                }
-                
-                while(currentY != targetY){
-                    List<Point> pList = map.getOrDefault(level, new ArrayList<>());
-                    pList.add(new Point(currentX, currentY));
-                    map.put(level++, pList);
-                    
-                    if(currentY > targetY){
-                        currentY--;
-                    } else{
-                        currentY++;
-                    }
-                }                
-                if(j == routes[i].length-1){
-                    List<Point> pList = map.getOrDefault(level, new ArrayList<>());
-                    pList.add(new Point(currentX, currentY));
-                    map.put(level, pList);
-                }
-            }
-        }
-        
-        for(Map.Entry<Integer, List<Point>> entry : map.entrySet()){
-            List<Point> list = entry.getValue();
-            Map<Point, Integer> pmap = new HashMap<>();
-            
-            // 중복되면 카운트 증가
-            for(Point p : list){
-                pmap.put(p, pmap.getOrDefault(p,0) + 1);
-            }
-            
-            for(Map.Entry<Point, Integer> pentry : pmap.entrySet()){
-                if(pentry.getValue() > 1){
-                    answer++;
-                }
-            }
-        }
+        bfs(points);
         
         return answer;
     }
     
-    
+    static void bfs(int[][] points){
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        
+    }
 }
